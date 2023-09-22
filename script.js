ffdc2eb66605d6aaa11c741d1a643cf8f5a204b0
abhin-2122003 <abhinshelly@gmail.com>
2023-09-22 13:55:30 +0530

    1st

---
+++ script.js
@@ -0,0 +1,151 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB-DAFObbV9nV4eBGirHnR15cqwB0GTvVM",
+    authDomain: "fir-dc-ae2fa.firebaseapp.com",
+    databaseURL: "https://fir-dc-ae2fa-default-rtdb.firebaseio.com",
+    projectId: "fir-dc-ae2fa",
+    storageBucket: "fir-dc-ae2fa.appspot.com",
+    messagingSenderId: "85786057242",
+    appId: "1:85786057242:web:913f4f11a98dd3b570cfa3",
+    measurementId: "G-5279S9TXH4"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+// Reference to the database node where you want to set the student counts
+const schoolsRef = ref(db, 'schools'); // Update this path if needed
+
+const studentCounts = {
+    "School A": {
+        "area X": 1000
+    },
+    "School B": {
+        "area Y": 900
+    },
+    "School C": {
+        "area Z": 2000
+    },
+    "School D": {
+        "area W": 1500
+    }
+};
+
+// Set the student counts in the database
+set(schoolsRef, studentCounts)
+    .then(() => {
+        console.log("Student counts have been set successfully.");
+    })
+    .catch((error) => {
+        console.error("Error setting student counts:", error);
+    });
+
+const policySelect = document.getElementById('policy');
+const form = document.getElementById('dataForm');
+
+// Reference to the database node containing student data
+const studentsRef = ref(db, 'students');
+let studentsData = {}; // To store the student data
+
+// Function to populate a <select> element with data
+function populateSelect(selectElement, data) {
+    selectElement.innerHTML = '';
+    data.forEach(item => {
+        const option = new Option(item, item);
+        selectElement.appendChild(option);
+    });
+}
+
+// Function to handle policy change
+function handlePolicyChange() {
+    const selectedPolicy = policySelect.value;
+
+    // Hide all selection options
+    ['school', 'area', 'gender', 'caste', 'age'].forEach(key => {
+        const selectDiv = document.getElementById(`${key}Select`);
+        selectDiv.style.display = 'none';
+    });
+
+    // Show the selected option
+    if (selectedPolicy !== '') {
+        const selectDiv = document.getElementById(`${selectedPolicy}Select`);
+        selectDiv.style.display = 'block';
+    }
+}
+
+// Attach event listeners
+policySelect.addEventListener('change', handlePolicyChange);
+
+// Fetch and populate data for each dropdown
+onValue(studentsRef, snapshot => {
+    studentsData = snapshot.val();
+    const ageSelect = document.getElementById('age');
+    const casteSelect = document.getElementById('caste');
+    const areaSelect = document.getElementById('area');
+    const genderSelect = document.getElementById('gender');
+    const schoolSelect = document.getElementById('school');
+
+    // Create arrays to store unique values for each field
+    const uniqueAges = [...new Set(Object.values(studentsData).map(student => student.age))];
+    const uniqueCastes = [...new Set(Object.values(studentsData).map(student => student.caste))];
+    const uniqueAreas = [...new Set(Object.values(studentsData).map(student => student.schoolArea))];
+    const uniqueGenders = [...new Set(Object.values(studentsData).map(student => student.gender))];
+    const uniqueSchools = [...new Set(Object.values(studentsData).map(student => student.schoolName))];
+
+    // Populate the dropdowns with unique values
+    populateSelect(ageSelect, uniqueAges);
+    populateSelect(casteSelect, uniqueCastes);
+    populateSelect(areaSelect, uniqueAreas);
+    populateSelect(genderSelect, uniqueGenders);
+    populateSelect(schoolSelect, uniqueSchools);
+});
+
+// Initial policy change to set up the form
+handlePolicyChange();
+
+// Function to calculate dropout rate based on the selected policy
+function calculateDropoutRate(selectedPolicy) {
+    const totalStudents = Object.values(studentsData).length;
+
+    let filteredStudents = Object.values(studentsData);
+
+    // Filter students based on the selected policy
+    if (selectedPolicy === 'school') {
+        const selectedSchool = document.getElementById('school').value; // Get the selected school
+        filteredStudents = filteredStudents.filter(student => student.schoolName === selectedSchool);
+    } else if (selectedPolicy === 'area') {
+        const selectedArea = document.getElementById('area').value; // Get the selected area
+        filteredStudents = filteredStudents.filter(student => student.schoolArea === selectedArea);
+    } else if (selectedPolicy === 'gender') {
+        const selectedGender = document.getElementById('gender').value; // Get the selected gender
+        filteredStudents = filteredStudents.filter(student => student.gender === selectedGender);
+    } else if (selectedPolicy === 'caste') {
+        const selectedCaste = document.getElementById('caste').value; // Get the selected caste
+        filteredStudents = filteredStudents.filter(student => student.caste === selectedCaste);
+    } else if (selectedPolicy === 'age') {
+        const selectedAge = document.getElementById('age').value; // Get the selected age
+        filteredStudents = filteredStudents.filter(student => student.age === selectedAge);
+    }
+
+    const dropoutCount = filteredStudents.filter(student => student.dropoutDate !== undefined).length;
+    const dropoutRate = (dropoutCount / totalStudents) * 100;
+
+    return dropoutRate;
+}
+
+// Function to update the UI with the calculated dropout rate
+function updateDropoutRate() {
+    const selectedPolicy = policySelect.value;
+    const dropoutRate = calculateDropoutRate(selectedPolicy);
+    const dropoutRateElement = document.getElementById('dropoutRate');
+    dropoutRateElement.textContent = `Dropout Rate: ${dropoutRate.toFixed(2)}%`;
+}
+
+// Attach an event listener to the policy dropdown to update the dropout rate
+policySelect.addEventListener('change', updateDropoutRate);
+
+// Initial update of dropout rate when the page loads
+updateDropoutRate();
